5d35a0f09852361549ba7eb9f38db354f7596ed6
S0n0f1saac <jtsfarrington@gmail.com>
2025-07-02 22:34:57 -0500

    set Times New Roman as global site font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body style={{ fontFamily: '"Times New Roman", serif' }}>
         <Navbar />
         {children}
       </body>
